2f76ddf381042b4e85ed19271f78967ef6f42270
Shaurya Choudhary <shaurya.src@gmail.com>
2021-03-21 20:52:13 +0530

    Define user module with authentication

--- quick_drop/quick_drop/urls.py
+++ quick_drop/quick_drop/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
