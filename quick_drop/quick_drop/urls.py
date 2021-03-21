b65c7732e0bdcb32b39e21adc3585313f8c0e0ff
Shaurya Choudhary <shaurya.src@gmail.com>
2021-03-21 20:57:32 +0530

    Fix errors

--- quick_drop/quick_drop/urls.py
+++ quick_drop/quick_drop/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 
+from . import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('users.urls')),
