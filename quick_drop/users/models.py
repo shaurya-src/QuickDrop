1a7829566d472480c0462dd3c21e10e8b7365558
Shaurya Choudhary <shaurya.src@gmail.com>
2021-05-01 09:16:37 +0530

    Remove all

--- quick_drop/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User , on_delete=models.CASCADE)
-    age = models.IntegerField(default=0)
-    desc = models.CharField(max_length=140,blank=True)
-    avatar = models.ImageField(upload_to='avatars/',default='avatars/default.png')
