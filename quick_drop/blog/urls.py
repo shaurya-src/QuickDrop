26cc4d618251e4824a8cac0660f7a8da2c19d7ce
Shaurya Choudhary <shaurya.src@gmail.com>
2021-05-08 09:13:13 +0530

    modify endpoints

--- quick_drop/blog/urls.py
+++ quick_drop/blog/urls.py
@@ -10,7 +10,8 @@ from .views import (
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
+    path('', views.index, name='blog-index'),
+    path('home/', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
